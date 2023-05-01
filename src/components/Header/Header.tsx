23259a1d81bf3fbe4e87e828be58c861c518f672
chris097 <chrisfidel.international@gmail.com>
2023-05-01 06:10:24 +0100

    homepage

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+
+type ListProps = {
+    name: string
+    id: number
+    path: string
+}
+
+const lists: ListProps[] = [
+    { id: 1, name: 'Home', path: '/' },
+    { id: 2, name: 'Portfolio', path: '/portfolio' },
+    { id: 3, name: 'About', path: '/about' },
+    { id: 4, name: 'Blog', path: '/blog' },
+];
+
+const Header = () => {
+    return (
+        <div className='border-b border-gray-800/50 bg-black w-full fixed top-0 left-0'>
+            <div className='w-[90%] mx-auto h-24 flex items-center justify-between'>
+                <div className='font-medium text-4xl'>CJC</div>
+                <ul className='flex gap-10 text-base'>
+                    {lists.map(list => (
+                        <li key={list.id} className='cursor-pointer'>
+                            <Link href={`${list.path}`}>{list.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default Header
