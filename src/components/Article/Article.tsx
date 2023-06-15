095d42bc9a6704ea8531877b8c9e708ad14ac6a7
Home <home@Homes-MacBook-Pro.local>
2023-06-15 08:32:58 +0100

    update app

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Article = () => {
@@ -8,7 +9,9 @@ const Article = () => {
         </p>
         <div className='md:text-md text-sm my-3 flex justify-between items-center'>
         <div><span className='mr-2'>2023-02-12</span> | <span className='ml-2'>5 mins Read</span></div>
-        <div className='text-xs bg-gray-800/60 w-12 h-6 rounded-full flex justify-center items-center cursor-pointer hover:opacity-60'>View</div>
+        <div className='text-xs bg-gray-800/60 w-12 h-6 rounded-full flex justify-center items-center cursor-pointer hover:opacity-60'>
+          <Link href="https://medium.com/@chrisfidel.international/generating-and-use-of-ssh-key-for-github-permission-denied-public-key-for-window-users-c19690d04ff0">View</Link>
+        </div>
         </div>
     </div>
   )
