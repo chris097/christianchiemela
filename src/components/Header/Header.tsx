97bdd3e2fae23e12016e4190458c7b6023eae900
Home <home@Homes-MacBook-Pro.local>
2023-06-19 08:06:36 +0100

    added mobile menu bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import {FiMenu }from 'react-icons/fi';
+import {TfiClose }from 'react-icons/tfi';
 
 type ListProps = {
     name: string
@@ -15,6 +17,7 @@ const lists: ListProps[] = [
 ];
 
 const Header = () => {
+    const [open, setOpen] = useState<boolean>(false);
     return (
         <div className='border-b border-gray-700/70 text-gray-100/90 bg-black w-full fixed top-0 left-0 z-50'>
             <div className='w-[80%] mx-auto h-24 flex items-center justify-between'>
@@ -26,6 +29,21 @@ const Header = () => {
                         </li>
                     ))}
                 </ul>
+                <div className='lg:hidden block'>
+                    <FiMenu size={20} onClick={() => setOpen(!open)} color='white' />
+                    {open && <div className='fixed flex top-0 left-0 inset-0 bg-black/90 overflow-hidden flex-col z-50'>
+                        <div onClick={() => setOpen(false)} className='flex justify-end m-10'>
+                            <TfiClose color='white' size={30} />
+                        </div>
+                         <ul className='gap-10 text-base z-50 relative opacity-100 flex mt-10 flex-col items-center h-full w-full'>
+                    {lists.map(list => (
+                        <li key={list.id} className='cursor-pointer text-white z-50 text-2xl'>
+                            <Link href={`${list.path}`}>{list.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+                    </div>}
+                </div>
             </div>
         </div>
     )
